0350c6e025333e920a2743771dfdb3c33de5dd31
Gurinder Singh <frederick.the.fool@gmail>
2012-04-03 06:29:57 +0530

    (Just for reference) added the softx86dbg cpp file that exercises the softx86 and softx87 libraries.

--- cpu.rb
+++ cpu.rb
@@ -4,6 +4,9 @@ class Cpu
   include Softx86
 
   def initialize
-    @softx86_ctx = Softx86::Softx86_ctx.new
+    @softx86_ctx = Softx86Ctx.new
+    softx86_init(@softx86_ctx, SX86_CPULEVEL_8086)
   end
 end
+
+c = Cpu.new

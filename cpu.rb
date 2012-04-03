811c8dbdebaa9795441088af2337f753536c0ab9
Gurinder Singh <frederick.the.fool@gmail>
2012-04-03 07:29:13 +0530

    Added some code in cpu.rb to initialize the softx86 processor.

--- cpu.rb
+++ cpu.rb
@@ -4,9 +4,38 @@ class Cpu
   include Softx86
 
   def initialize
-    @softx86_ctx = Softx86Ctx.new
-    softx86_init(@softx86_ctx, SX86_CPULEVEL_8086)
+    @ctx = Softx86Ctx.new
+    callbacks = Softx86Callbacks.new
+
+    callbacks.on_read_memory = method(:on_read_memory).to_proc
+    callbacks.on_write_memory = method(:on_write_memory).to_proc
+    callbacks.on_read_io = method(:on_read_io).to_proc
+    callbacks.on_write_io = method(:on_write_io).to_proc
+
+#    @ctx[:callbacks] = callbacks
+
+    if !softx86_init(@ctx, SX86_CPULEVEL_8086)
+      raise RuntimeError "Failed to initialize underlying library."
+    end
   end
+
+
+  private
+
+  def on_read_memory(ctx, address, buf, buf_size)
+  end
+
+  def on_write_memory(ctx, address, buf, buf_size)
+  end
+
+  def on_read_io(ctx, address, buf, buf_size)
+  end
+
+  def on_write_io(ctx, address, buf, buf_size)
+  end
+
+  
+
 end
 
 c = Cpu.new

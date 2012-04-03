e5a650a0b760f3c64840266d7d17c90ed05f2636
Gurinder Singh <gurry_uor@yahoo.co.uk>
2012-04-03 16:34:03 +0530

    Resolved the problem of assigning to the members of the 'callbacks' struct in the 'ctx'

--- cpu.rb
+++ cpu.rb
@@ -7,12 +7,10 @@ class Cpu
     @ctx = Softx86Ctx.new
     callbacks = Softx86Callbacks.new
 
-    callbacks.on_read_memory = method(:on_read_memory).to_proc
-    callbacks.on_write_memory = method(:on_write_memory).to_proc
-    callbacks.on_read_io = method(:on_read_io).to_proc
-    callbacks.on_write_io = method(:on_write_io).to_proc
-
-#    @ctx[:callbacks] = callbacks
+    @ctx[:callbacks][:on_read_memory] = method(:on_read_memory).to_proc
+    @ctx[:callbacks][:on_write_memory] = method(:on_write_memory).to_proc
+    @ctx[:callbacks][:on_read_io] = method(:on_read_io).to_proc
+    @ctx[:callbacks][:on_write_io] = method(:on_write_io).to_proc
 
     if !softx86_init(@ctx, SX86_CPULEVEL_8086)
       raise RuntimeError "Failed to initialize underlying library."

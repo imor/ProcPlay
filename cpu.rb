92ddf4a7567fb962331072d544d3c494ed6276e7
Gurinder Singh (XGSI) <xgsi@saxobank.com>
2012-04-04 17:15:17 +0530

    Added some more code to cpu.rb

--- cpu.rb
+++ cpu.rb
@@ -26,7 +26,7 @@ class Cpu
     softx86_reset(@ctx.pointer)
     LibC::memset(@ram, 0, RAM_SIZE)
     
-    softx86_set_instruction_ptr(@ctx.pointer, SEGMENTS_BASE, 0x0)
+    softx86_set_instruction_ptr(@ctx.pointer, SEGMENTS_BASE, )
 		softx86_set_stack_ptr(@ctx.pointer, SEGMENTS_BASE, 0x0)
 		softx86_setsegval(@ctx.pointer, SX86_SREG_DS, SEGMENTS_BASE)
 		softx86_setsegval(@ctx.pointer, SX86_SREG_ES, SEGMENTS_BASE)
@@ -35,12 +35,7 @@ class Cpu
 
   # Runs the given sequence of instructions. Multiple instructions should be separated by semicolon (;)
   def run(instruction_sequence)
-    # we must fake INT 21h (located at 9000:FFF0) so we must do our own work
-    if @ctx[:state][:segment_reg][SX86_SREG_CS][:val] == 0x9000 && @ctx[:state][:reg_ip] == 0xFFF0
-      soft_int21(@ctx.pointer);
-    end
-
-    # execute whatever is there at the current IP
+    # Execute whatever is there at the current IP
     if !softx86_step(@ctx.pointer)
       raise RuntimeError "Error while executing instructions"
     end
@@ -52,7 +47,7 @@ class Cpu
 
   RAM_SIZE = 1024 * 1024 # 1 MB
   SEGMENTS_BASE = 0x1010
-  
+  SP_BASE = 
   def on_read_memory(ctx, address, buf, buf_size)
     if address >= RAM_SIZE
       cpsize = 0

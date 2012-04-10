9ff5d8edbd5bd71f4cd71586151eb4bab2712015
Gurinder Singh <frederick.the.fool@gmail.com>
2012-04-10 14:32:51 +0530

    Added assembler library I created by hacking NASM code. Added the corresponding (unfinished) ffi wrapper.

---
+++ asm.rb
@@ -0,0 +1,16 @@
+require 'ffi'
+
+module Asm
+	extend FFI::Library
+	
+	if FFI::Platform.unix?
+		ffi_lib "lib/libasm.so"
+	elsif FFI::Platform.windows?
+		# TODO: Add support for Windows.
+		#ffi_lib "lib/asm.dll"
+	else
+		raise RuntimeError "Platform not supported. Only windows or unix platforms are supported."
+	end
+
+  attach_function :assemble_instruction, [ :string, :pointer ], :ulong # TODO: confirm that the return type should is indeed ulong
+end

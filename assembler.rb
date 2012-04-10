d223d78a7822368f0b4de8fd61ce4cefc487eb59
Gurinder Singh <frederick.the.fool@gmail.com>
2012-04-10 14:34:18 +0530

    Commented out code in Assembler that calls FASM. Readying it to call libasm instead.

--- assembler.rb
+++ assembler.rb
@@ -1,18 +1,20 @@
-require './fasm'
+# require './fasm'
+require './asm'
 require './libc'
 
 module Assembler
 	def assemble(instructions)
-		buf_size = 0x800000 # Saw this size used in the demo provided with fasm library
-		buffer = LibC::malloc BUF_SIZE
-		max_no_of_passes = 100 # This is the recommended value in fasm library documentation
-		output_pipe = nil # We don't want no output to any pipe
-		status = Fasm::fasm_Assemble(instructions, buffer, BUF_SIZE, output_pipe)
+			
+		# buf_size = 0x800000 # Saw this size used in the demo provided with fasm library
+		# buffer = LibC::malloc BUF_SIZE
+		# max_no_of_passes = 100 # This is the recommended value in fasm library documentation
+		# output_pipe = nil # We don't want no output to any pipe
+		# status = Fasm::fasm_Assemble(instructions, buffer, BUF_SIZE, output_pipe)
 
-		if status != Fasm::FASM_OK
-			raise RuntimeError "Failed to assemble instruction #{instruction}"
-		end
+		# if status != Fasm::FASM_OK
+		# 	raise RuntimeError "Failed to assemble instruction #{instruction}"
+		# end
 
-		return buffer[:output_data].read_string()
+		# return buffer[:output_data].read_string()
 	end
 end
